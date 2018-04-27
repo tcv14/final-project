751df0bac83a7be64c3d18f1da0df60b8b7e3ceb
Tiffany Cheng <tycheng@bu.edu>
2018-04-27 00:33:25 -0400

    tidied data and started first graphs of word frequency

--- de data analysis.R
+++ de data analysis.R
@@ -2,24 +2,117 @@ library(tidyverse)
 library(tidytext)
 #install.packages("lsa")
 library(lsa)
+#install.packages("quanteda")
+library(quanteda)
 
+# load stop words
+data(stop_words)
+stopwords_lsa <- as_data_frame(stopwords_de)
+colnames(stopwords_lsa) <- "word"
+stopwords_quanteda <- as_data_frame(stopwords("german"))
+colnames(stopwords_quanteda) <- "word"
+own_stopwords.de <- tibble(
+  `word` = c("facebook", "mark", "zuckerberg", "zuckerbergs", "cambridge", "analytica", "us",
+              "nun", "dabei", "dafür", "darauf", "hätten", "deren", "ja", "eigentlich",
+              "kommt", "mal", "heute", "gerade", "schon", "warum", "sagt", "beim", "gibt",
+              "gestern", "marc"))
+
+#########################
+### For News Articles ###
+#########################
+
+# load news article data
 load("./News Article Data/news.german.RData")
 
-stopwords_lsa <- as_data_frame(stopwords_de)
-own_stopwords.de <- tibble(
-  `value` = c("facebook", "mark", "zuckerberg", "zuckerbergs", "cambridge", "analytica", "er", "us",
-              "will", "man", "damit", "ob", "nun", "dabei", "dafür", "darauf", "hätten",
-              "deren", "doch", "dies", "also"))
-
-news.german.words <- news.german %>% unnest_tokens(value, value) %>%
+# clean news article words
+news.german.words <- news.german %>%
+  unnest_tokens(word, value, token = "regex", pattern = "’") %>%
+  unnest_tokens(word, word, token = "regex", pattern = "'") %>%
+  unnest_tokens(word, word) %>%
+  mutate(word = gsub('[[:digit:]]+', '', word)) %>%
+  mutate(word = gsub('[[:punct:] ]+', '', word)) %>%
+  filter(word != '') %>%
   anti_join(stopwords_lsa) %>%
+  anti_join(stopwords_quanteda) %>%
   anti_join(own_stopwords.de) %>%
-  mutate(value = replace(value, value == "nutzern", "nutzer")) %>%
-  mutate(value = replace(value, value == "fragen", "frage")) %>%
-  mutate(value = replace(value, value == "informationen", "information")) %>%
-  mutate(value = replace(value, value == "firmen", "firma"))
+  anti_join(stop_words) %>%
+  mutate(word = replace(word, word == "nutzern", "nutzer")) %>%
+  mutate(word = replace(word, word == "fragen", "frage")) %>%
+  mutate(word = replace(word, word == "informationen", "information")) %>%
+  mutate(word = replace(word, word == "firmen", "firma"))
 
-häufigkeit <- news.german.words %>% count(value, sort = TRUE)
+# find highest frequency words
+news_frequency.de <- news.german.words %>% count(word, sort = TRUE)
+
+# plot highest frequency words
+plot_frequency.de <- news_frequency.de %>%
+  filter(n > quantile(n, 0.99)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+###################
+### For Twitter ###
+###################
+
+# load twitter data
+load("./Twitter Data/tweet.german.RData")
+
+# clean twitter data- removing links, but retaining hashtags and mentions
+replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+unnest_reg <- "([^A-Za-zäüöß_\\d#@']|'(?![A-Za-zäüöß_\\d#@]))"
+tweet.all_german.words <- tweet.german %>% 
+  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = "’") %>%
+  unnest_tokens(word, word, token = "regex", pattern = "'") %>%
+  unnest_tokens(word, word, token = "regex", pattern = unnest_reg) %>%
+  mutate(word = gsub('[[:digit:]]+', '', word)) %>%
+  filter(word != '') %>%
+  anti_join(stopwords_lsa) %>%
+  anti_join(stopwords_quanteda) %>%
+  anti_join(own_stopwords.de) %>%
+  anti_join(stop_words)
+
+# finding most commonly used hashtags
+tweet.hashtag_frequency.de <- tweet.all_german.words %>% 
+  filter(word == str_extract_all(word, "#\\S+")) %>% 
+  count(word, sort = TRUE)
+
+# plot most commonly used hashtags
+plot_tweet.hashtag_frequency.de <- tweet.hashtag_frequency.de %>%
+  filter(n > quantile(n, 0.95)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+# finding most commonly used mentions
+tweet.mention_frequency.de <- tweet.all_german.words %>% 
+  filter(word == str_extract_all(word, "@\\S+")) %>% 
+  count(word, sort = TRUE)
+
+# plot most commonly used mentions
+plot_tweet.mention_frequency.de <- tweet.mention_frequency.de %>%
+  filter(n > quantile(n, 0.9)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+# clean twitter data, but removing all links, hashtags, and mentions
+tweet.german.words <- tweet.all_german.words %>%
+  mutate(word = str_replace_all(word, "#\\S+", "")) %>%
+  mutate(word = str_replace_all(word, "@\\S+", "")) %>%
+  filter(word != '')
+
+# finding most commonly used words outside of hashtags and mentions
+tweet_frequency.de <- tweet.german.words %>% count(word, sort = TRUE)
+
+# plot most commonly used words outside of hashtags and mentions
+plot_tweet_frequency.de <- tweet_frequency.de %>%
+  filter(n > quantile(n, 0.99)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+
+##########################
+### Sentiment Analysis ###
+##########################
 
 # Wörter laden und vorbereiten
 sent <- c(
--- en data analysis.R
+++ en data analysis.R
@@ -1,19 +1,97 @@
 library(tidyverse)
 library(tidytext)
+library(stringr)
 
-load("./News Article Data/news.english.RData")
-
+# load stop words
 data(stop_words)
 
+# create own list of stop words based on subject content
 own_stopwords.en <- tibble(
-  `word` = c("facebook", "mark", "zuckerberg", "cambridge", "analytica"))
+  `word` = c("facebook", "mark", "zuckerberg", "cambridge", "analytica", "fb", "didn", "don", "doesn", "ve"))
 
-news.english.words <- news.english %>% unnest_tokens(word, value, token = "regex", pattern = "’") %>%
+#########################
+### For News Articles ###
+#########################
+
+# load news article data
+load("./News Article Data/news.english.RData")
+
+# clean news article words
+news.english.words <- news.english %>% 
+  unnest_tokens(word, value, token = "regex", pattern = "’") %>%
   unnest_tokens(word, word, token = "regex", pattern = "'") %>%
   unnest_tokens(word, word) %>%
+  mutate(word = gsub('[[:digit:]]+', '', word)) %>%
+  mutate(word = gsub('[[:punct:] ]+', '', word)) %>%
+  filter(word != '') %>%
   anti_join(stop_words) %>%
   anti_join(own_stopwords.en) %>%
   mutate(word = replace(word, word == "users", "user")) %>%
   mutate(word = replace(word, word == "companies", "company"))
 
-frequency <- news.english.words %>% count(word, sort = TRUE)
+# find highest frequency words
+news_frequency.en <- news.english.words %>% count(word, sort = TRUE)
+
+# plot highest frequency words
+plot_frequency.en <- news_frequency.en %>%
+  filter(n > quantile(n, 0.99)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+###################
+### For Twitter ###
+###################
+
+# load twitter data
+load("./Twitter Data/tweet.english.RData")
+
+# clean twitter data- removing links, but retaining hashtags and mentions
+replace_reg <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|&amp;|&lt;|&gt;|RT|https"
+unnest_reg <- "([^A-Za-z_\\d#@']|'(?![A-Za-z_\\d#@]))"
+tweet.all_english.words <- tweet.english %>% 
+  mutate(text = str_replace_all(text, replace_reg, "")) %>%
+  unnest_tokens(word, text, token = "regex", pattern = "’") %>%
+  unnest_tokens(word, word, token = "regex", pattern = "'") %>%
+  unnest_tokens(word, word, token = "regex", pattern = unnest_reg) %>%
+  mutate(word = gsub('[[:digit:]]+', '', word)) %>%
+  filter(word != '') %>%
+  anti_join(stop_words) %>%
+  anti_join(own_stopwords.en)
+
+# finding most commonly used hashtags
+tweet.hashtag_frequency.en <- tweet.all_english.words %>% 
+  filter(word == str_extract_all(word, "#\\S+")) %>% 
+  count(word, sort = TRUE)
+
+# plot most commonly used hashtags
+plot_tweet.hashtag_frequency.en <- tweet.hashtag_frequency.en %>%
+  filter(n > quantile(n, 0.995)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+# finding most commonly used mentions
+tweet.mention_frequency.en <- tweet.all_english.words %>% 
+  filter(word == str_extract_all(word, "@\\S+")) %>% 
+  count(word, sort = TRUE)
+
+# plot most commonly used mentions
+plot_tweet.mention_frequency.en <- tweet.mention_frequency.en %>%
+  filter(n > quantile(n, 0.995)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
+# clean twitter data, but removing all links, hashtags, and mentions
+tweet.english.words <- tweet.all_english.words %>%
+  mutate(word = str_replace_all(word, "#\\S+", "")) %>%
+  mutate(word = str_replace_all(word, "@\\S+", "")) %>%
+  filter(word != '')
+
+# finding most commonly used words outside of hashtags and mentions
+tweet_frequency.en <- tweet.english.words %>% count(word, sort = TRUE)
+
+# plot most commonly used words outside of hashtags and mentions
+plot_tweet_frequency.en <- tweet_frequency.en %>%
+  filter(n > quantile(n, 0.999)) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) + geom_col() + xlab(NULL) + coord_flip()
+
