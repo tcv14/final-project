09bf83d22309df13ef6be51e0d6ac1f5ce6e9d5c
Tiffany Cheng <tycheng@bu.edu>
2018-08-22 15:30:33 -0400

    data collection

--- 1. Data Collection.R
+++ 1. Data Collection.R
@@ -16,55 +16,55 @@ my_oauth <- setup_twitter_oauth(api_key, api_secret, access_token, access_token_
 # search English tweets with the hashtag "MarkZuckerberg"
 #facebook_en <- searchTwitter('#MarkZuckerberg -filter:retweets', n=10000, lang="en")
 #facebook_en <- twListToDF(facebook_en)
-save(facebook_en, file = "./Twitter Data/facebook_en.Rdata")
+#save(facebook_en, file = "./Twitter Data/facebook_en.Rdata")
 load("./Twitter Data/facebook_en.RData")
 
 # search English tweets with the hashtag "Zuckerberg"
 #facebook_en2 <- searchTwitter('#Zuckerberg -filter:retweets', n=10000, lang="en")
 #facebook_en2 <- twListToDF(facebook_en2)
-save(facebook_en2, file = "./Twitter Data/facebook_en2.Rdata")
+#save(facebook_en2, file = "./Twitter Data/facebook_en2.Rdata")
 load("./Twitter Data/facebook_en2.RData")
 
 # search English tweets with the hashtag "FacebookDataBreach"
 #facebook_en3 <- searchTwitter('#FacebookDataBreach -filter:retweets', n=5000, lang="en")
 #facebook_en3 <- twListToDF(facebook_en3)
-save(facebook_en3, file = "./Twitter Data/facebook_en3.Rdata")
+#save(facebook_en3, file = "./Twitter Data/facebook_en3.Rdata")
 load("./Twitter Data/facebook_en3.RData")
 
 # search English tweets with the hashtag "Facebook" and the word "Zuckerberg" in the tweet
 #facebook_en4 <- searchTwitter('#Facebook + Zuckerberg -filter:retweets', n=15000, lang="en")
 #facebook_en4 <- twListToDF(facebook_en4)
-save(facebook_en4, file = "./Twitter Data/facebook_en4.Rdata")
+#save(facebook_en4, file = "./Twitter Data/facebook_en4.Rdata")
 load("./Twitter Data/facebook_en4.RData")
 
 # search German tweets with the hashtag "MarkZuckerberg"
 #facebook_de <- searchTwitter('#MarkZuckerberg -filter:retweets', n=1500, lang="de")
 #facebook_de <- twListToDF(facebook_de)
-save(facebook_de, file = "./Twitter Data/facebook_de.Rdata")
+#save(facebook_de, file = "./Twitter Data/facebook_de.Rdata")
 load("./Twitter Data/facebook_de.RData")
 
 # search German tweets with the hashtag "Zuckerberg"
 #facebook_de2 <- searchTwitter('#Zuckerberg -filter:retweets', n=1500, lang="de")
 #facebook_de2 <- twListToDF(facebook_de2)
-save(facebook_de2, file = "./Twitter Data/facebook_de2.Rdata")
+#save(facebook_de2, file = "./Twitter Data/facebook_de2.Rdata")
 load("./Twitter Data/facebook_de2.RData")
 
 # search German tweets with the hashtag "FacebookDataBreach"
 #facebook_de3 <- searchTwitter('#FacebookDataBreach -filter:retweets', n=1500, lang="de")
 #facebook_de3 <- twListToDF(facebook_de3)
-save(facebook_de3, file = "./Twitter Data/facebook_de3.Rdata")
+#save(facebook_de3, file = "./Twitter Data/facebook_de3.Rdata")
 load("./Twitter Data/facebook_de3.RData")
 
 # search German tweets with the hashtag "Datenskandal"
 #facebook_de4 <- searchTwitter('#Datenskandal -filter:retweets', n=1500, lang="de")
 #facebook_de4 <- twListToDF(facebook_de4)
-save(facebook_de4, file = "./Twitter Data/facebook_de4.Rdata")
+#save(facebook_de4, file = "./Twitter Data/facebook_de4.Rdata")
 load("./Twitter Data/facebook_de4.RData")
 
 # search German tweets with the hashtag "Facebook" and the word "Zuckerberg" in the tweet
 #facebook_de5 <- searchTwitter('#Facebook + Zuckerberg -filter:retweets', n=1000, lang="de")
 #facebook_de5 <- twListToDF(facebook_de5)
-save(facebook_de5, file = "./Twitter Data/facebook_de5.Rdata")
+#save(facebook_de5, file = "./Twitter Data/facebook_de5.Rdata")
 load("./Twitter Data/facebook_de5.RData")
 
 # bind all english tweets together, only keeping the text portion
